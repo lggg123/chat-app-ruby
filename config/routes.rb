a5feb10d81e79e360bc9d6f3cd941a6e97f068ce
lggg123 <machinelearner2334@gmail.com>
2022-04-21 13:49:11 -0700

    changed index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'landing/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :messages, only: [:new, create]
+  resources :messages, only: [:new, :create]
 
   root 'landing#index'
 end
